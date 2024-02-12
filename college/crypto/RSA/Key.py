cc36a7c1572a620e328dc77038e002a95c599df2
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-12 11:55:57 +0200

    Refactor names in keyGenerator, KeyGenerator -> Key

--- college/crypto/RSA/Key.py
+++ college/crypto/RSA/Key.py
@@ -1,19 +1,15 @@
 import os
 from Crypto.PublicKey import RSA
 
-class KeyGenerator:
+class Key:
     def __init__(self, keyPath : str) -> None:
         self.keyPath = keyPath
     
-    def fileExists(self):
+    def exists(self):
         return os.path.isfile(self.keyPath)
     
-    def createKey(self):
-        pwd = input("Введіть пароль для вашого ключа: ")
-        self.generateKey(pwd)
-        print(f"Ваш ключ успішно записано до {self.keyPath}")
 
-    def generateKey(self, password):
+    def generate(self, password):
         with open(self.keyPath, "wb") as f:
             key = RSA.generate(3072)
             data = key.export_key(passphrase=password,
@@ -21,3 +17,8 @@ class KeyGenerator:
                                 protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
                                 prot_params={'iteration_count':131072})
             f.write(data)
+    
+    def createFromTerminal(self):
+        pwd = input("Введіть пароль для вашого ключа: ")
+        self.generate(pwd)
+        print(f"Ваш ключ успішно записано до {self.keyPath}")
