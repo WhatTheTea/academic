32a4387ad516becba4e049483d16441bbc93a1df
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-12 11:30:17 +0200

    Refactor main to Program class

--- college/crypto/RSA/main.py
+++ college/crypto/RSA/main.py
@@ -1,43 +1,35 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-KEY_FILE = "key.pem"
+from keyGenerator import KeyGenerator
 
-def createKey():
-    pwd = input("Введіть пароль для вашого ключа: ")
-    generateKey(pwd)
-    print(f"Ваш ключ успішно записано до {KEY_FILE}")
-
-def generateKey(password):
-    with open(KEY_FILE, "wb") as f:
-        key = RSA.generate(3072)
-        data = key.export_key(passphrase=password,
-                              pkcs=8,
-                              protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
-                              prot_params={'iteration_count':131072})
-        f.write(data)
-        
-def encryptMessage():
+class Program:
+    _keyGenerator : KeyGenerator
     
-    pass
+    def setKeyGenerator(self, kg : KeyGenerator):
+        self._keyGenerator = kg
+        return self
+    
+    def mainloop(self):
+        while True:      
+            print(
+                    """МЕНЮ:
+                    1. Створити ключ
+                    2. Зашифрувати повідомлення
+                    3. Розшифрувати повідомлення
+                    """
+                )
+            match(input()):
+                case "1":
+                    self._keyGenerator.createKey()
+                case "2":
+                    pass
+                case "3":
+                    pass
+                case _:
+                    print("invalid option")
+                    exit()
 
 if __name__ == '__main__':
-    while True:
-        print(
-            """МЕНЮ:
-            1. Створити ключ
-            2. Зашифрувати повідомлення
-            3. Розшифрувати повідомлення
-            """
-        )
-        match(input()):
-            case "1":
-                createKey()
-                pass
-            case "2":
-                pass
-            case "3":
-                pass
-            case _:
-                print("invalid option")
-                exit()
+    program = Program().setKeyGenerator(KeyGenerator("key.pem"))
+    program.mainloop()
