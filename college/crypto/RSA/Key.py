eba993ad50adf741f7057b18db384bd1d4f166f1
WhatTheTea <silchenko.whatthetea@gmail.com>
2024-02-12 13:37:56 +0200

    Change exporting private to public key

--- college/crypto/RSA/Key.py
+++ college/crypto/RSA/Key.py
@@ -9,16 +9,13 @@ class Key:
         return os.path.isfile(self.keyPath)
     
 
-    def generate(self, password):
+    def generate(self):
         with open(self.keyPath, "wb") as f:
             key = RSA.generate(3072)
-            data = key.export_key(passphrase=password,
-                                pkcs=8,
-                                protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
-                                prot_params={'iteration_count':131072})
+            data = key.public_key().export_key()
             f.write(data)
     
     def createFromTerminal(self):
-        pwd = input("Введіть пароль для вашого ключа: ")
-        self.generate(pwd)
+        # pwd = input("Введіть пароль для вашого ключа: ")
+        self.generate()
         print(f"Ваш ключ успішно записано до {self.keyPath}")
