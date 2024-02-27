367135acdca798f811b90634479b6b1dce028c4f
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-02-27 21:46:53 +0200

    Reformat college/cpp/lr14

--- college/cpp/LR14/lr14.cpp
+++ college/cpp/LR14/lr14.cpp
@@ -1,12 +1,12 @@
 #include <cmath>
 #include <functional>
 #include <iostream>
-#include <ostream>
 #include <string>
 #include <vector>
 
 using std::cout, std::cin, std::string, std::vector;
-template<typename T>
+
+template <typename T>
 static void map(vector<vector<T>> &matrix,
                 std::function<void(T *, int, int)> fun) {
   for (int i = 0; i < matrix.size(); i++) {
@@ -21,11 +21,13 @@ int main() {
   while (true) {
     cout << "n - число. e - вихід\n";
     cin >> choice;
-    if (choice == 'e') break;
+    if (choice == 'e')
+      break;
     vector<vector<double>> p(choice - '0', vector<double>(choice - '0', 0.0));
     map<double>(p, [](double *v, int i, int j) { *v = sin(i + j); });
     int pos = 0, neg = 0;
-    map<double>(p, [&pos, &neg](double *v, int i, int j) { *v > 0 ? pos++ : neg++;});
-    cout <<"Додатніх: "<< pos << " Від'ємних: " << neg << '\n';
+    map<double>(
+        p, [&pos, &neg](double *v, int i, int j) { *v > 0 ? pos++ : neg++; });
+    cout << "Додатніх: " << pos << " Від'ємних: " << neg << '\n';
   }
 }
