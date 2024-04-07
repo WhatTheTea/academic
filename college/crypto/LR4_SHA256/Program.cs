b825296584746ce1a29093f0ad9904f16405fe22
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-08 02:11:22 +0300

    Remove allocation from lr4 crypto

--- college/crypto/LR4_SHA256/Program.cs
+++ college/crypto/LR4_SHA256/Program.cs
@@ -2,6 +2,6 @@
 using System.Text;
 
 var data = Console.ReadLine();
-var bytes = new UTF8Encoding().GetBytes(data);
+var bytes = UTF8Encoding.Default.GetBytes(data!);
 var hash = SHA256.HashData(bytes);
 Console.WriteLine(string.Join("", hash.Select(x => x.ToString("x"))));
