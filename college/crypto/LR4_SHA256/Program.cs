dc7780c4b26d302abbb1b19486eb1905d08acaf6
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-08 02:09:31 +0300

    Add crypto lr4

---
+++ college/crypto/LR4_SHA256/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var data = Console.ReadLine();
+var bytes = new UTF8Encoding().GetBytes(data);
+var hash = SHA256.HashData(bytes);
+Console.WriteLine(string.Join("", hash.Select(x => x.ToString("x"))));
