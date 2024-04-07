c4cde88fe0eab6364adba5c4f564da14aa10a423
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-08 02:00:54 +0300

    Implement crypto lr2

--- college/crypto/LR2_MD4_5/Program.cs
+++ college/crypto/LR2_MD4_5/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var data = Console.ReadLine();
+var bytes = new UTF8Encoding().GetBytes(data);
+var hash = MD5.HashData(bytes);
+Console.WriteLine(string.Join("", hash.Select(x => x.ToString("x"))));
