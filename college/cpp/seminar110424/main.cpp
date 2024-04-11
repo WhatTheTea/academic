704183b3ab14a4ed4980bd23f13c8ddf877f535d
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-11 08:21:10 +0300

    Finish cpp seminar

--- college/cpp/seminar110424/main.cpp
+++ college/cpp/seminar110424/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
+    double R = 0, S_circle = 0, S_sphere = 0;
+    double pi = 3.14159;
+    std::cout << "Радіус: ";
+    std::cin >> R;
+    S_circle = pi * R * R;
+    S_sphere = 4 * pi * R * R;
+    printf("Площа круга: %.2f\nПлоща сфери: %.2f\n", S_circle, S_sphere);
+
+    system("pause");
     return 0;
 }
